6f3feca20e4aa732ee424024924aaf14b1c58e18
suman-mk <sumanmalik@Sumans-MacBook-Pro.local>
2021-09-02 00:26:30 -0400

    third commit

---
+++ map.js
@@ -0,0 +1,24 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀" + `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🙁" + `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//The map function will return a new array based on the results of the callback function.
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  newArray = []
+  for(i =0; i < array.length; i++){
+    newArray.push(callback(array[i]))
+  }
+  return newArray
+}
+
+const results1 = map(words, word => word[0]);
+
+assertEqual(results1, ["g", "c", "t", "m", "t"])
---
+++ takeUntil.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀" + `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🙁" + `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+
+const takeUntil = function(array, callback) {
+  let sliceEndIndex = "";
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      sliceEndIndex = i;
+      newArray = array.slice(0, sliceEndIndex);
+      return newArray;
+    }
+  }
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertEqual(results1, [1, 2, 5, 7, 2]);
+assertEqual(results2, ["I've", "been", "to", "Hollywood"]);
