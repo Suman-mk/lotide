501181eb8ed6aa929ce68c0c39147c421ab2a62d
suman-mk <sumanmalik@Sumans-MacBook-Pro.local>
2021-09-03 19:30:04 -0400

    fifth commit

--- tail.js
+++ tail.js
@@ -16,7 +16,7 @@ const assertEqual = function(array, expected) {
 const headlast = function (arr) {
   let head = [];
   for (let i=0; i<arr.length; i++) {
-    if (i !== 0) {                                 //checks the first element in the array
+    if (i !== 0) {                                 //checks the last element in the array
       head.push(arr[i])
       
     }
